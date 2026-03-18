e05766de7e381e4c38859edbb000e06e38c9a947
YoLaub <johngreta904@gmail.com>
2026-03-18 15:58:38 +0100

    test: update Vite configuration to exclude e2e tests and node_modules

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/__tests__/setup.ts'],
     css: false,
+    exclude: ['e2e/**', 'node_modules/**'],
     typecheck: {
       tsconfig: './tsconfig.test.json',
     },
